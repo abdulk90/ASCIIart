00c1209403a1a01c7baf8aeca55d0a6edd7de9f9
abdulk90 <abdul.kayyali@gmail.com>
2016-04-05 15:23:03 -0400

    Reworked Iteration2, added default font

--- app.rb
+++ app.rb
@@ -2,7 +2,16 @@ require "sinatra"
 require "sinatra/reloader"
 require "artii"
 
-get "/ascii/:style/:random_word" do
+
+get "/ascii/:random_word" do
+
+	my_artii = Artii::Base.new :font => "doh"
+	@word = my_artii.asciify(params[:random_word])
+	erb :worb
+
+end
+
+get "/ascii/:random_word/:style" do
 
 	my_artii = Artii::Base.new :font => params[:style]
 	@word = my_artii.asciify(params[:random_word])
