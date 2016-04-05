39ecd882b5156dd7fa4d0b6b81c1ba1f7e96a323
abdulk90 <abdul.kayyali@gmail.com>
2016-04-05 15:16:12 -0400

    pug and panda-fied

--- app.rb
+++ app.rb
@@ -2,12 +2,18 @@ require "sinatra"
 require "sinatra/reloader"
 require "artii"
 
-get "/ascii/:random_word" do
+get "/ascii/:style/:random_word" do
 
-	my_artii = Artii::Base.new :font => 'avatar'
+	my_artii = Artii::Base.new :font => params[:style]
 	@word = my_artii.asciify(params[:random_word])
 	erb :worb
 
 end
 
-	
+get "/ascii/pug/special/secret" do
+	erb :pug
+end
+
+get "/ascii/panda/special/secret" do
+	erb :panda
+end
